43d724f0714494a8a34c508dbaed22f4b82f6d1a
yildirimu92-cpu <yildirim.u92@gmail.com>
2026-08-05 13:22:48 +0200

    chore: refresh customer call action assets

--- customer-dashboard/shared/customer-runtime-design-foundation.js
+++ customer-dashboard/shared/customer-runtime-design-foundation.js
@@ -12,7 +12,7 @@
     '/shared/customer-navigation-components.css?v=20260803-1',
     '/shared/customer-settings-components.css?v=20260803-2',
     '/shared/customer-support-components.css?v=20260802-2',
-    '/shared/customer-call-log-components.css?v=20260805-1'
+    '/shared/customer-call-log-components.css?v=20260805-2'
   ];
 
   stylesheets.forEach((href) => {
@@ -44,8 +44,8 @@
   };
 
   loadSequentialScripts([
-    '/shared/customer-call-view-model.js?v=20260805-3',
-    '/shared/customer-call-log-model.js?v=20260805-3',
+    '/shared/customer-call-view-model.js?v=20260805-4',
+    '/shared/customer-call-log-model.js?v=20260805-4',
     '/shared/customer-runtime-call-log-owner.js?v=20260805-3'
   ]);
 
